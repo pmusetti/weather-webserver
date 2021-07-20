5ff17ccdb9ce3239523816c4ab636a40bce88a33
Pablo Musetti <pmusettimillan@gmail.com>
2021-07-20 01:17:09 -0300

    asdf

--- public/js/app.js
+++ public/js/app.js
@@ -10,36 +10,36 @@ const msg_7 = document.querySelector('#msg-07')
 const msg_8 = document.querySelector('#msg-08')
 const msg_9 = document.querySelector('#msg-09')
 const msg_10 = document.querySelector('#msg-10')
-const msg_11= document.querySelector('#msg-11')
-const msg_12= document.querySelector('#msg-12')
+const msg_11 = document.querySelector('#msg-11')
+const msg_12 = document.querySelector('#msg-12')
 const img = document.querySelector('#icon')
 const myLocation = document.querySelector('#myLocation')
 
 
 //Se utilizara en heroku por tanto se elimina la primer parte de la url http://192.168.1.47:3000
-function getForecast(location){
+function getForecast(location) {
   console.log(location)
-  fetch('/weather?address=' + location).then((response)=>{
-    response.json().then((res)=>{
-      if(res.error){
+  fetch('/weather?address=' + location).then((response) => {
+    response.json().then((res) => {
+      if (res.error) {
         msg_one.textContent = 'ERROR!'
         msg_two.textContent = res.error
         msg_three.textContent = 'oops!'
-      }else{
-        msg_1.textContent = 'Location: '+ res.data.location
-        msg_2.textContent= res.data.resume.toUpperCase()
-        msg_3.textContent = 'Temperature: ' + res.data.temp 
-        msg_4.textContent = 'Feels like: ' + res.data.feels_like 
-        msg_5.textContent = 'Temp min: ' + res.data.temp_min 
-        msg_6.textContent = 'Temp max: ' + res.data.temp_max 
-        msg_7.textContent = 'Pressure: ' + res.data.pressure 
+      } else {
+        msg_1.textContent = 'Location: ' + res.data.location
+        msg_2.textContent = res.data.resume.toUpperCase()
+        msg_3.textContent = 'Temperature: ' + res.data.temp
+        msg_4.textContent = 'Feels like: ' + res.data.feels_like
+        msg_5.textContent = 'Temp min: ' + res.data.temp_min
+        msg_6.textContent = 'Temp max: ' + res.data.temp_max
+        msg_7.textContent = 'Pressure: ' + res.data.pressure
         msg_8.textContent = 'Humidity: ' + res.data.humidity
         msg_9.textContent = 'Wind speed: ' + res.data.wind_speed
-        msg_10.textContent =  'Clouds: ' + res.data.clouds
-        msg_11.textContent =  'Sunrise: ' + res.data.sunrise
-        msg_12.textContent =  'Sunset: ' + res.data.sunset
-        img.src = 'http://openweathermap.org/img/wn/'+res.data.icon+'@2x.png'
-       
+        msg_10.textContent = 'Clouds: ' + res.data.clouds
+        msg_11.textContent = 'Sunrise: ' + res.data.sunrise
+        msg_12.textContent = 'Sunset: ' + res.data.sunset
+        img.src = 'http://openweathermap.org/img/wn/' + res.data.icon + '@2x.png'
+
       }
     })
   })
@@ -48,13 +48,13 @@ function getForecast(location){
 
 
 
-weatherForm.addEventListener('submit', (e)=>{
+weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()//Evita el comportamiento por defecto que es recargar la pagina.
   const location = search.value
   getForecast(location)
-  })
+})
 
-myLocation.addEventListener('onClick',(e) =>{
+myLocation.addEventListener('click', (e) => {
   e.preventDefault()
 
   var options = {
@@ -62,20 +62,20 @@ myLocation.addEventListener('onClick',(e) =>{
     timeout: 5000,
     maximumAge: 0
   };
-  
+
   function success(pos) {
     var crd = pos.coords;
-  
+
     console.log('Your current position is:');
     console.log('Latitude : ' + crd.latitude);
     console.log('Longitude: ' + crd.longitude);
     console.log('More or less ' + crd.accuracy + ' meters.');
   };
-  
+
   function error(err) {
     console.warn('ERROR(' + err.code + '): ' + err.message);
   };
-  
+
   navigator.geolocation.getCurrentPosition(success, error, options);
-  alert("my location is " ) 
+  alert("my location is ")
 })
