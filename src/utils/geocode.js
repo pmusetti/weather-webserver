06493b04ac185fbaed80de77026ecbfbf7569d10
Pablo Musetti <pmusettimillan@gmail.com>
2021-07-20 00:11:17 -0300

    asdf

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -52,11 +52,11 @@ var requestOptions = {
 
 fetch("https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey=cf92bd8c57cc47a78a06128662042303", requestOptions)
   .then(response => response.json())
-  .then(result => data = {
+  .then(result => {data = {
     latitud: -34,
     longitud: -54,
     location: "La Paz"
-  })
+  }})
   .catch(error => console.log('error', error));
 
 
