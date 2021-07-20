93de76ba2ef27de26ee9167f888085fd629b586a
Pablo Musetti <pmusettimillan@gmail.com>
2021-07-20 00:15:53 -0300

    asdf

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -49,15 +49,15 @@ const request = require('request')
 var requestOptions = {
   method: 'GET',
 };
-
-fetch("https://api.geoapify.com/v1/geocode/search?text=38%20Upper%20Montagu%20Street%2C%20Westminster%20W1H%201LJ%2C%20United%20Kingdom&apiKey=cf92bd8c57cc47a78a06128662042303", requestOptions)
+const geocode = (address, callback)=>{
+fetch("https://api.geoapify.com/v1/geocode/search?text="+ encodeURIComponent(address) +"&apiKey=cf92bd8c57cc47a78a06128662042303", requestOptions)
   .then(response => response.json())
-  .then(result => {data = {
+  .then((result) => {data = {
     latitud: -34,
     longitud: -54,
     location: "La Paz"
   }})
   .catch(error => console.log('error', error));
 
-
+}
 module.exports = geocode
