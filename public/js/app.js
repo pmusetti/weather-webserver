ad08624317eb5fa9238761c0b9bb7f56f6d54530
Pablo Musetti <pmusettimillan@gmail.com>
2021-07-20 01:41:22 -0300

    asdf

--- public/js/app.js
+++ public/js/app.js
@@ -71,7 +71,7 @@ myLocation.addEventListener('click', (e) => {
     console.log('Latitude : ' + crd.latitude);
     console.log('Longitude: ' + crd.longitude);
     console.log('More or less ' + crd.accuracy + ' meters.');
-    alert('Location : ' + crd.latitude + ' ' + crd.longitude + ' with acuracy ' + crd.accuracy/1000 + 'more or less kilometers' )
+    alert('Location : ' + crd.latitude + ' ' + crd.longitude + ' with accuracy ' + crd.accuracy/1000 + '  kilometers' )
   };
 
   function error(err) {
