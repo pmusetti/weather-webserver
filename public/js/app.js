929b5c54b8787dab780a40b23fc934d471722545
Pablo Musetti <pmusettimillan@gmail.com>
2021-07-27 23:06:35 -0300

    nuevo html

--- public/js/app.js
+++ public/js/app.js
@@ -11,8 +11,8 @@ const feelsLikeField = document.querySelector('#feelsLikeField')
 // const humField = document.querySelector('#humField')
 // const windSpeedField = document.querySelector('#windSpeedField')
 // const cloudsField = document.querySelector('#cloudsField')
-// const sunriseField = document.querySelector('#sunriseField')
-// const sunsetField = document.querySelector('#sunsetField')
+const sunriseField = document.querySelector('#sunriseField')
+const sunsetField = document.querySelector('#sunsetField')
 const img = document.querySelector('#icon')
 //const myLocation = document.querySelector('#myLocation')
 //const accuracyField = document.querySelector('#accuracyField')
@@ -37,8 +37,8 @@ function getForecast(location) {
         // humField.textContent = 'Humidity: ' + res.data.humidity
         // windSpeedField.textContent = 'Wind speed: ' + res.data.wind_speed
         // cloudsField.textContent = 'Clouds: ' + res.data.clouds
-        // sunriseField.textContent = 'Sunrise: ' + res.data.sunrise
-        // sunsetField.textContent = 'Sunset: ' + res.data.sunset
+        sunriseField.textContent = 'Sunrise: ' + res.data.sunrise
+        sunsetField.textContent = 'Sunset: ' + res.data.sunset
         img.src = 'http://openweathermap.org/img/wn/' + res.data.icon + '@2x.png'
         //img.src = 'https://camo.githubusercontent.com/f422ba9e472321d1107866a3320d77932f393646e40a73ecc8646a1b8a31aeb4/68747470733a2f2f626d63646e2e6e6c2f6173736574732f776561746865722d69636f6e732f76322e302f66696c6c2f636c6561722d6461792e737667'
 
