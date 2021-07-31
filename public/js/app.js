1220119537895aabad3f7c752c04fb5bc4935b5f
Pablo Musetti <pmusettimillan@gmail.com>
2021-07-31 00:26:03 -0300

    se agrego campo para seleccion de ciudad en header

--- public/js/app.js
+++ public/js/app.js
@@ -42,7 +42,7 @@ function getForecast(location) {
         sunsetField.textContent = 'Puesta: ' + res.data.sunset
         img.src = 'http://openweathermap.org/img/wn/' + res.data.icon + '@2x.png'
         //img.src = 'https://camo.githubusercontent.com/f422ba9e472321d1107866a3320d77932f393646e40a73ecc8646a1b8a31aeb4/68747470733a2f2f626d63646e2e6e6c2f6173736574732f776561746865722d69636f6e732f76322e302f66696c6c2f636c6561722d6461792e737667'
-        
+
       }
     })
   })
@@ -55,10 +55,11 @@ weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()//Evita el comportamiento por defecto que es recargar la pagina.
   const location = search.value
   var acc = document.querySelector("accordion")
-    var panel = document.querySelector(".panel");
-    panel.style.display = "none"
-    acc.style.display = "block"
+  var panel = document.querySelector(".panel");
+
   getForecast(location)
+  panel.style.display = "none"
+  acc.style.display = "block"
 })
 
 resumeField.addEventListener('click', (e) => {
@@ -96,15 +97,15 @@ forecast = () => {
 forecast();
 
 
-  acc.addEventListener("click", (e) => {
-    var acc = document.querySelector("accordion")
-    var panel = document.querySelector(".panel");
-    panel.style.display = "block"
-    acc.style.display = "none"
-    // if (panel.style.display === "block") {
-    //   panel.style.display = "none";
-    // } else {
-    //   panel.style.display = "block";
-    // }
-  })
+acc.addEventListener("click", (e) => {
+  var acc = document.querySelector("accordion")
+  var panel = document.querySelector(".panel");
+  panel.style.display = "block"
+  acc.style.display = "none"
+  // if (panel.style.display === "block") {
+  //   panel.style.display = "none";
+  // } else {
+  //   panel.style.display = "block";
+  // }
+})
 
