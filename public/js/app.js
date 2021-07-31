1863bb602e4cac7ad5ca7afb245069b110c484c1
Pablo Musetti <pmusettimillan@gmail.com>
2021-07-30 21:26:54 -0300

    se agrego campo para seleccion de ciudad en header

--- public/js/app.js
+++ public/js/app.js
@@ -99,7 +99,7 @@ alert(acc + " " + img)
 
   acc.addEventListener("click", (e) => {
     //this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    var panel = document.querySelector(".panel");
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
