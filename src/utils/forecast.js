dffe54996b664e2ff8efb993744b4042ceec67ca
Pablo Musetti <pmusettimillan@gmail.com>
2022-01-27 00:32:38 -0300

    asdf

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -78,14 +78,14 @@ const forecast = (data, callback) => {
         
       }
 
-      for (let i = 0; i < response.body.daily.length -1; i++) {
+      /*for (let i = 0; i < response.body.daily.length -1; i++) {
         data.daily[i].min_temp = response.body.daily[i].temp.min.toFixed(0);
         data.daily[i].max_temp= response.body.daily[1].temp.max.toFixed(0);
         data.daily[i].resume= response.body.daily[1].weather[0].description;
         data.daily[i].icon= response.body.daily[1].weather[0].icon;
         data.daily[i].date= response.body.daily[1].sunset;
         
-      }
+      }*/
 
       callback(undefined, data)
     }
