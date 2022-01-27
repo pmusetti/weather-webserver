c57ec13d7be14f3bfc85ee803c1b4cceb0b0c4bf
Pablo Musetti <pmusettimillan@gmail.com>
2022-01-27 00:46:18 -0300

    asdf

--- public/js/app.js
+++ public/js/app.js
@@ -90,20 +90,17 @@ function getForecast(location) {
         day1_temp.textContent = res.data.daily[0].min_temp + "/" + res.data.daily[0].max_temp + "ºC"
         day1_resume.textContent = res.data.daily[0].resume
         day1_icon.src = 'http://openweathermap.org/img/wn/' + res.data.daily[0].icon + '@2x.png'
-        let date = new Date(res.data.daily[0].date)
-        day1_date.textContent = date.getDate() + "/" + (date.getMonth() +1)
+        day1_date.textContent = res.data.daily[0].date
 
         day2_temp.textContent = res.data.daily[1].min_temp + "/" + res.data.daily[1].max_temp + "ºC"
         day2_resume.textContent = res.data.daily[1].resume
         day2_icon.src = 'http://openweathermap.org/img/wn/' + res.data.daily[1].icon + '@2x.png'
-        date = new Date(res.data.daily[1].date)
-        day2_date.textContent = date.getDate() + "/" + (date.getMonth() +1)
+        day2_date.textContent = res.data.daily[0].date
 
         day3_temp.textContent = res.data.daily[2].min_temp + "/" + res.data.daily[2].max_temp + "ºC"
         day3_resume.textContent = res.data.daily[2].resume
         day3_icon.src = 'http://openweathermap.org/img/wn/' + res.data.daily[2].icon + '@2x.png'
-        date = new Date(res.data.daily[2].date)
-        day3_date.textContent = date.getDate() + "/" + (date.getMonth() +1)
+        day3_date.textContent = res.data.daily[0].date
 
         
       }
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -83,7 +83,8 @@ const forecast = (data, callback) => {
         data.daily[i].max_temp= response.body.daily[i].temp.max.toFixed(0);
         data.daily[i].resume= response.body.daily[i].weather[0].description;
         data.daily[i].icon= response.body.daily[i].weather[0].icon;
-        data.daily[i].date= response.body.daily[i].sunset;
+        let date = new Date(response.body.daily[i].sunrise)
+        data.daily[i].date= date.getDay() + "/" + date.getMonth();
         
       }
 
